303da26866b73223dd99b68387cf5df5250e189c
Kauan Pacheco <kauanpacheco.dev@gmail.com>
2024-02-20 00:07:53 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDatahora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongTimeString(),
+                Hora = DateTime.Now.ToShortDateString(),
+            };
+            return Ok(obj);
+        }
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var mensagem = $"Ola {nome}, Seja Bem-Vindo!";
+            return Ok(new {mensagem});
+        }
+    }
+}
